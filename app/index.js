99afb7fe0873b83a291ecb453295d47ee2c0e1d3
Gil Barbara <gilbarbara@gmail.com>
2013-12-20 01:40:13 -0200

    helo

---
+++ app/index.js
@@ -0,0 +1,131 @@
+"use strict";
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var WebGenerator = module.exports = function WebGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(WebGenerator, yeoman.generators.Base);
+
+WebGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+  console.log('HTML5 Boilerplate, jQuery and Bootstrap 3 are included out of the box.');
+
+  var prompts = [{
+    name: 'appname',
+    message: 'What\'s the name of this project?'
+  },
+  {
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+      name: 'Modernizr',
+      value: 'includeModernizr',
+      checked: true
+    },
+    {
+      name: 'Underscore',
+      value: 'includeUnderscore',
+      checked: true
+    },
+    {
+      name: 'Respond',
+      value: 'includeRespond',
+      checked: true
+    },
+    {
+        name: 'FontAwesome',
+        value: 'includeFontAwesome',
+        checked: true
+    },
+    {
+      name: 'LessHat',
+      value: 'includeLesshat',
+      checked: true
+    }]
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.appname = props.appname;
+    
+    var features = props.features;
+
+    function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.includeModernizr = hasFeature('includeModernizr');
+    this.includeUnderscore = hasFeature('includeUnderscore');
+    this.includeRespond = hasFeature('includeRespond');
+    this.includeLesshat = hasFeature('includeLesshat');
+    this.includeFontAwesome = hasFeature('includeFontAwesome');
+
+    cb();
+  }.bind(this));
+};
+
+WebGenerator.prototype.gruntfile = function gruntfile() {
+  this.template('Gruntfile.js');
+};
+
+WebGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
+
+WebGenerator.prototype.git = function git() {
+  this.copy('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+};
+
+WebGenerator.prototype.bower = function bower() {
+  this.copy('_bower.json', 'bower.json');
+};
+
+WebGenerator.prototype.jshint = function jshint() {
+  this.copy('jshintrc', '.jshintrc');
+};
+
+WebGenerator.prototype.editorConfig = function editorConfig() {
+  this.copy('editorconfig', '.editorconfig');
+};
+
+WebGenerator.prototype.h5bp = function h5bp() {
+  this.copy('favicon.ico', 'assets/favicon.ico');
+  this.copy('robots.txt', 'assets/robots.txt');
+  this.copy('htaccess', '.htaccess');
+};
+
+WebGenerator.prototype.mainStylesheet = function mainStylesheet() {
+  this.copy('main.less', 'assets/styles/main.less');
+};
+
+WebGenerator.prototype.app = function app() {
+  this.mkdir('assets');
+  this.mkdir('assets/scripts');
+  this.mkdir('assets/styles');
+  this.mkdir('assets/images');
+  this.copy('index.html', 'index.html');
+  this.copy('main.js', 'assets/scripts/main.js');
+  this.copy('mixins.js', 'assets/scripts/mixins.js');
+};
+
+WebGenerator.prototype.install = function () {
+  if (this.options['skip-install']) {
+    return;
+  }
+
+  var done = this.async();
+  this.installDependencies({
+    skipInstall: this.options['skip-install'],
+    callback: done
+  });
+};
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,207 @@
+// Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%%= pkg.title || pkg.name %> - v<%%= pkg.version %> - ' +
+            '<%%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '* Copyright (c) <%%= grunt.template.today("yyyy") %>;' +
+            ' Licensed <%%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        // Project settings
+        yeoman: {
+            // Configurable paths
+            app: 'assets',
+            dist: 'dist'
+        },
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            js: {
+                files: ['<%%= yeoman.assets %>/scripts/{,*/}*.js'],
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            less: {
+				files: '<%%= yeoman.assets %>/styles/main.less',
+				tasks: ['less']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%%= yeoman.assets %>/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    '<%%= yeoman.assets %>/images/{,*/}*.{gif,jpeg,jpg,png,svg,webp}'
+                ]
+            }
+        },
+
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                // Change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.tmp',
+                        '<%%= yeoman.assets %>'
+                    ]
+                }
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base: '<%%= yeoman.dist %>',
+                    livereload: false
+                }
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%%= yeoman.dist %>/*',
+                        '!<%%= yeoman.dist %>/.git*'
+                    ]
+                }]
+            },
+            ext: ['<%%= concat.ext.dest %>'],
+            server: '.tmp'
+        },
+		less: {
+			options: {
+				paths: ['<%%= yeoman.assets %>/styles'],
+				yuicompress: true
+			},
+			files: {
+				'<%%= yeoman.dist %>/main.min.css': '<%%= yeoman.assets %>/styles/main.less'
+			}
+		},
+        // Make sure code styles are up to par and there are no obvious mistakes
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js',
+                '<%%= yeoman.assets %>/scripts/{,*/}*.js'
+            ]
+        },
+        // Copies remaining files to places other tasks can use
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: '<%%= yeoman.assets %>',
+                        dest: '<%%= yeoman.dist %>',
+                        src: [
+                            '*.{ico,png,txt}',
+                            '.htaccess',
+                            'images/{,*/}*.webp',
+                            '{,*/}*.html'
+                        ]
+                    },
+                    {expand: true, flatten: true, src: ['bower_components/bootstrap/dist/css/bootstrap.min.css'], dest: '<%%= yeoman.dist %>', filter: 'isFile'}<% if (includeRespond) { %>,
+                    {expand: true, flatten: true, src: ['bower_components/respond/respond.min.js'], dest: '<%%= yeoman.dist %>', filter: 'isFile'}<% } %><% if (includeFontAwesome) { %>,
+                    {expand: true, cwd: 'bower_components/', src: ['font-awesome/css/*', 'font-awesome/fonts/*'], dest: '<%%= yeoman.dist %>'}<% } %>
+                ]
+            }
+        },
+        concat: {
+            options: {
+                banner: '<%%= banner %>',
+                stripBanners: true
+            },
+            ext: {
+                src: [
+                    'bower_components/jquery/jquery.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.min.js'<% if (includeUnderscore) { %>,
+                    'bower_components/underscore/underscore.js',
+                    'assets/js/mixins.js'<% } %>
+                ],
+                dest: '<%%= yeoman.dist %>/extensions.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%%= banner %>'
+            },
+            main: {
+                src: '<%%= yeoman.assets %>/scripts/main.js',
+                dest: '<%%= yeoman.dist %>/main.min.js'
+            },
+            ext: {
+                src: '<%%= concat.ext.dest %>',
+                dest: '<%%= yeoman.dist %>/extensions.min.js'
+            }
+        }<% if (includeModernizr) { %>,
+        // Generates a custom Modernizr build that includes only the tests you
+        // reference in your app
+        modernizr: {
+            devFile: 'bower_components/modernizr/modernizr.js',
+            outputFile: '<%%= yeoman.dist %>/modernizr.js',
+            files: [
+                '<%%= yeoman.dist %>/{,*/}*.js',
+                '<%%= yeoman.dist %>/{,*/}*.css'
+            ],
+            uglify: true
+        }<% } %>
+    });
+
+    grunt.registerTask('serve', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'connect:livereload',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('server', function () {
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve']);
+    });
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'concat',
+        'uglify',
+        'clean:ext',
+        'copy'<% if (includeModernizr) { %>,
+        'modernizr'<% } %>
+    ]);
+
+    grunt.registerTask('default', [
+        'less',
+        'jshint',
+        'uglify:main'
+    ]);
+};
